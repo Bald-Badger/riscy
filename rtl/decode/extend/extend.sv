8d23022b96e65ba270a0d6939e4cde9e433310e9
Shuai <szhang565@wisc.edu>
2021-02-02 03:39:57 -0600

    Update extend.sv

--- rtl/decode/extend/extend.sv
+++ rtl/decode/extend/extend.sv
@@ -7,14 +7,14 @@ module extend (
     
 	wire[6:0] opcode = instr[6:0];
 	 
-	assign imm =	(opcpde == `LUI)	? {instr[31:12], 12'b0}:
-					(opcpde == `AUIPC) 	? {instr[31:12], 12'b0}:
-					(opcpde == `JAL) 	? {32'd4}: // rd = pc + 4
-					(opcpde == `JALR) 	? {32'd4}: // rd = pc + 4
-					(opcpde == `B) 		? {instr[31]*20, instr[7], instr[30:25], instr[11:8], 0}:
-					(opcpde == `LOAD) 	? {instr[31]*20, instr[31:20]}:
-					(opcpde == `STORE) 	? {instr[31]*20, instr[31:25], instr[11:7]}:
-					(opcpde == `I) 		? {instr[31]*20, instr[31:20]}:
+	assign imm =	(opcode == `LUI)	? {instr[31:12], 12'b0}:
+					(opcode == `AUIPC) 	? {instr[31:12], 12'b0}:
+					(opcode == `JAL) 	? {32'd4}: // rd = pc + 4
+					(opcode == `JALR) 	? {32'd4}: // rd = pc + 4
+					(opcode == `B) 		? {instr[31]*20, instr[7], instr[30:25], instr[11:8], 0}:
+					(opcode == `LOAD) 	? {instr[31]*20, instr[31:20]}:
+					(opcode == `STORE) 	? {instr[31]*20, instr[31:25], instr[11:7]}:
+					(opcode == `I) 		? {instr[31]*20, instr[31:20]}:
 					`NULL;
 
 endmodule
