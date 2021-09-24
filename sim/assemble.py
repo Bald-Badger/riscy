aa21fe16efc6652cb5b075871ff3ea48d851db2e
Bald Badger <32609230+Bald-Badger@users.noreply.github.com>
2021-09-24 17:57:45 -0400

    cloned riscv python assembler

--- sim/assemble.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-from riscv_assembler.convert import AssemblyConverter
-
-if __name__ == '__main__':
-	cnv = AssemblyConverter(output_type="p")  # just printing
-	cnv.convert("instr.s")
