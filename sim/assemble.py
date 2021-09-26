fcf31012e947acc09b36977e3d37312d29b040ad
Bald Badger <32609230+Bald-Badger@users.noreply.github.com>
2021-09-26 17:13:14 -0400

    stable release of rars, and new assembler script

--- sim/assemble.py
+++ sim/assemble.py
@@ -4,10 +4,18 @@ risc-v assembler, convert a .s file into .asm machine code
 
 from riscv_assembler.convert import AssemblyConverter
 import shutil
+import os
 
 in_name = 'instr.s'
 middle_name = 'instr.txt'
 out_name = 'instr.asm'
+
+def old_main():
+    cnv = AssemblyConverter(output_type='tp', hexMode=True)
+    cnv.convert(in_name)
+    move_file()
+    clean()
+    process_file()
 
 
 def clean():
@@ -32,16 +40,25 @@ def process_file():
         lines[i] = lines[i][2:]
     f = open(out_name, 'w')
     f.writelines(lines)
-    zeros = ['00000000\n']*10
+    f.close()
+    append_zeros()
+
+
+def append_zeros():
+    f = open(out_name, 'a')
+    zeros = ['00000000\n'] * 10
     f.writelines(zeros)
     f.close()
 
 
+def assemble():
+    example = "java -jar rars.jar dump 0x00400000-0x00401000 " \
+              "HexText instr.asm instr.s"
+    addr = "0x00400000-0x00401000"
+    cmd = "java -jar rars.jar dump " + addr + " HexText " + out_name + " " + in_name
+    os.system(cmd)
+
+
 if __name__ == '__main__':
-    cnv = AssemblyConverter(output_type='tp', hexMode=True)
-    cnv.convert(in_name)
-    move_file()
-    clean()
-    process_file()
-
-
+    assemble()
+    append_zeros()
