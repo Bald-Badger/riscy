800e47a81151a62c9e4f6150bf0b00c5e6aa2803
Luke <32609230+Bald-Badger@users.noreply.github.com>
2021-12-17 06:11:27 -0500

    wrap up

--- rtl/proc.sv
+++ rtl/proc.sv
@@ -5,6 +5,7 @@
 // synopsys translate_on
 
 import defines::*;
+`include "../formal/riscv-formal/checks/rvfi_macros.vh"
 
 module proc(
 	input	logic 			clk,			// clock from PLL, frequency is defines::FREQ
@@ -97,6 +98,9 @@ module proc(
 		mem_ex_fwd_data = wb_data;
 		mem_mem_fwd_data = wb_data;
 	end
+
+	// instruction order
+	data_t		instr_order;
 
 
 	// fetch stage	
@@ -419,6 +423,30 @@ end
 
 //////////////////////// formal verification start /////////////////////////////
 
+//// RVFI interface ////
+logic [`RISCV_FORMAL_NRET                        - 1 : 0] rvfi_valid;		// valid instruction
+logic [`RISCV_FORMAL_NRET *                 64   - 1 : 0] rvfi_order;		// programmer's instruction order
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_ILEN   - 1 : 0] rvfi_insn;		// retired instruction word
+logic [`RISCV_FORMAL_NRET                        - 1 : 0] rvfi_trap;		// instruction cannot be decoded
+logic [`RISCV_FORMAL_NRET                        - 1 : 0] rvfi_halt;		// last instruction
+logic [`RISCV_FORMAL_NRET                        - 1 : 0] rvfi_intr;		// must be set for the first instruction that is part of a trap handler
+logic [`RISCV_FORMAL_NRET *                  2   - 1 : 0] rvfi_mode;		// 0=U-Mode, 1=S-Mode, 2=Reserved, 3=M-Mode
+logic [`RISCV_FORMAL_NRET *                  2   - 1 : 0] rvfi_ixl;			// 1=32, 2=64
+logic [`RISCV_FORMAL_NRET *                  5   - 1 : 0] rvfi_rs1_addr;	// rs1 addr for retired instr
+logic [`RISCV_FORMAL_NRET *                  5   - 1 : 0] rvfi_rs2_addr;	// rs2 addr for retired instr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs1_rdata;	// value of rs1 right before retired instr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rs2_rdata;	// value of rs2 right before retired instr
+logic [`RISCV_FORMAL_NRET *                  5   - 1 : 0] rvfi_rd_addr;		// rd addr for retired instr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_rd_wdata;	// value of rd right after retired instr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_pc_rdata;	// address of the retired instruction
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_pc_wdata;	// address of the next instruction
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_mem_addr;	// mem access addr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN/8 - 1 : 0] rvfi_mem_rmask;	// which bytes in rvfi_mem_rdata contain valid read data from rvfi_mem_addr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN/8 - 1 : 0] rvfi_mem_wmask;	// which bytes in rvfi_mem_wdata contain valid data that is written to rvfi_mem_addr
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_mem_rdata;	// data being read by retired instruction
+logic [`RISCV_FORMAL_NRET * `RISCV_FORMAL_XLEN   - 1 : 0] rvfi_mem_wdata;	// data being written by retired instruction
+///////////////////////
+
 //// legal instruction assertion ////
 logic instr_legal_f, instr_legal_d, instr_legal_x, instr_legal_m, instr_legal_w;
 
@@ -486,5 +514,15 @@ assert property (instr_m_legal_property)
 assert property (instr_w_legal_property)
 	else $warning("instr_w_legal_property failed at %t",$time());
 /////////////////////////////////////
+
+always_ff @(posedge clk or negedge rst_n) begin
+	if (~rst_n) begin
+		instr_order <= NULL;
+	end else if (instr_legal_w && instr_w != NOP && ~stall_mem_wb) begin
+		instr_order <= instr_order + 1;
+	end else begin
+		instr_order <= instr_order;
+	end
+end
 
 endmodule : proc
