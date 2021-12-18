3cc259214e395c1bd8f902187578cdb7d262c2a2
Hongtao Cai <55171255+Capstone-only@users.noreply.github.com>
2021-12-18 17:50:24 -0500

    Add files via upload

--- rtl/execute/execute.sv
+++ rtl/execute/execute.sv
@@ -39,7 +39,7 @@ module execute (
 		mul_busy		=	(instr.funct7 == M_INSTR) &&
 							(mul_instr) &&
 							(~mul_result_valid) &&
-							(instr.opcode == R);
+							(instr.opcode == R); 
 		execute_busy	=	mul_busy || div_busy;
 	end
 
@@ -77,5 +77,57 @@ module execute (
 		.div_result_valid	(div_result_valid),
 		.mul_result_valid	(mul_result_valid)
 	);
+
+	property mul_div_not_equal;
+		@(posedge clk) ((instr.opcode == R) && (instr.funct7 == M_INSTR)) |-> (div_result_valid && (~mul_result_valid))
+	endproperty 
+	
+	assume property (mul_div_not_equal) //make sure div_result_valid is not equal when multly and divide
+	else begin
+		$display("property mul and div not match");
+	end;
+/////////////////////////////////////////////////////////////
+	/*property test;
+		@(posedge clk) (((instr.opcode == R) ) |-> (execute_busy == 0))
+	endproperty 
+
+	assume property (test)
+	else begin
+		$display("execute_busy is %d, instr_funct7 is %d " ,execute_busy, instr.funct7);
+	end;*/
+
+	property property_ex_busy;
+		@(posedge clk) ((instr.opcode !=R) |-> (execute_busy == 0))
+	endproperty
+
+	assume property (property_ex_busy) //make sure when instr.opcode !=R, exexcute_busy will keep 0
+	else begin
+		$display("property_ex_busy not match"); 
+	end;
+///////////////////////////////////////////////////////////
+	property property_ex_busy_div;
+		@(posedge clk) (((instr.opcode == R) && (instr.funct7 == M_INSTR) && (instr.funct3[2] == 1)) |->
+						##[0:11] (execute_busy == 1))
+	endproperty
+
+	assume property(property_ex_busy_div) //make sure when divide, under 12 cycles, exectue_busy is always 1
+	else begin
+		$display("property_ex_busy_div not match, execute_busy is %d", execute_busy);
+	end;
+
+	property property_ex_busy_mul;
+		@(posedge clk) (((instr.opcode == R) && (instr.funct7 == M_INSTR) && (instr.funct3[2] == 0)) |->
+						##[0:5] (execute_busy == 1))
+	endproperty
+
+	assume property(property_ex_busy_mul) //make sure when multiply, under 6 cycles, exectue_busy is always 1
+	else begin
+		$display("property_ex_busy_mul not match, execute_busy is %d", execute_busy);
+	end;
+
+
+
+
+
 	
 endmodule : execute
