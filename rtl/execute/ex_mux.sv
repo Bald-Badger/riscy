8d9a1d88c28eaa1ce45128ccebc6d9106ddb1958
Hongtao Cai <55171255+Capstone-only@users.noreply.github.com>
2021-12-22 12:53:58 -0500

    Add files via upload

--- rtl/execute/ex_mux.sv
+++ rtl/execute/ex_mux.sv
@@ -132,61 +132,259 @@ module ex_mux (
 			end
 		endcase
 	end
-//((opcode_formal == R) && (instr[31:25] != M_INSTR)) |->
-					//((c_out_formal == c_out) && (rd_wr));
-
-	//opcode_t		opcode_formal;
-
-	//always_comb begin: formal
-		//opcode_formal = opcode_t'(instr[6:0]); 
-	//end
 
 
-	//property a_out_general;
-		//((a_out == rs1_mux_out) || (a_out == ex_ex_fwd_data) || (a_out == mem_ex_fwd_data) || (a_out == NULL));
-	//endproperty assert property(a_out_general); //make sure a_out comes from one of the fwd_a mux input
+	opcode_t		opcode_formal;
 
-	//property b_out_general;
-		//((b_out == rs2_mux_out) || (b_out == ex_ex_fwd_data) || (b_out == mem_ex_fwd_data) || (b_out == NULL));
-	//endproperty
+	always_comb begin: formal
+		opcode_formal = opcode_t'(instr[6:0]); 
+	end
 
-	/*assert property(b_out_general); //make sure a_out comes from one of the fwd_a mux input
+	always_comb begin: a_out_formal
+		ex_mux_a_out_general: assert ((a_out == NULL) 
+									|| (a_out == rs1_mux_out) || 
+									(a_out == ex_ex_fwd_data) || 
+									(a_out == mem_ex_fwd_data)) begin
+			$display("ex-mux: a_out general case pass");
+		end else begin
+			$display("WB: a_out general case doesn't pass");
+		end
+			 
+	end //automatic
 
+	always_comb begin: b_out_formal
+		ex_mux_b_out_general: assert ((b_out == NULL) 
+									|| (b_out == rs2_mux_out) || 
+									(b_out == ex_ex_fwd_data) || 
+									(b_out == mem_ex_fwd_data)) begin
+			$display("ex-mux: b_out general case pass");
+		end else begin
+			$display("WB: b_out general case doesn't pass");
+		end
 
+	end //automatic
 
+	always_comb begin: rs1_mux_out_formal
+		ex_mux_rs1_out_general: assert ((rs1_mux_out == NULL) 
+									||(rs1_mux_out == rs1) || 
+										(rs1_mux_out == pc)) begin
+			$display("ex-mux: ex_mux_rs1_out_general case pass");
+		end else begin
+			$display("WB: ex_mux_rs1_out_general case doesn't pass");
+		end
 
+	end //automatic
 
-	property rs1_mux_out_prop;
-		((rs1_mux_out == NULL) || (rs1_mux_out == rs1) || (rs1_mux_out == pc));
-	endproperty
-
-	assert property(rs1_mux_out_prop); //make sure rs1_out rs1_mux_out comes from one of the rs1_mux input
+	always_comb begin: rs2_mux_out_formal
+		ex_mux_rs2_out_general: assert ((rs2_mux_out == NULL) 
+									||(rs2_mux_out == rs2) || 
+										(rs2_mux_out == imm)) begin
+			$display("ex-mux: ex_mux_rs2_out_general general case pass");
+		end else begin
+			$display("WB: ex_mux_rs2_out_general case doesn't pass");
+		end
 	
-	property rs2_mux_out_prop;
-		((rs2_mux_out == NULL) || (rs2_mux_out == rs2) || (rs2_mux_out == imm));
-	endproperty
+	end //automatic
 
-	assert property(rs2_mux_out_prop); //make sure rs1_out rs1_mux_out comes from one of the rs1_mux input
 
+
+	/*
 	//RS_EX_SEL: 2'b00
 	//MEM_EX_SEL: 2'b01
-	//WB_EX_SEL: 2'b10
+	//WB_EX_SEL: 2'b10 */
 
+	always_comb begin: opcode_rs1_mux_out_formal
+		case (opcode_formal)
+			R: begin
+				R_rs1_mux_output: assert (rs1_mux_out == rs1) begin
+					$display("ex-mux: R type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: R type rs1_mux_output don't pass");
+				end
+			end
+			
+			I: begin
+				I_rs1_mux_output: assert (rs1_mux_out == rs1) begin
+					$display("ex-mux: I type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: I type rs1_mux_output don't pass");
+				end
+			end
+
+			B: begin
+				B_rs1_mux_output: assert (rs1_mux_out == rs1) begin
+					$display("ex-mux: B type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: B type rs1_mux_output don't pass");
+				end
+			end
+
+			LUI: begin
+				LUI_rs1_mux_output: assert (rs1_mux_out == NULL) begin
+					$display("ex-mux: LUI type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: LUI type rs1_mux_output don't pass");
+				end				
+			end
+
+			AUIPC: begin
+				AUIPC_rs1_mux_output: assert (rs1_mux_out == pc) begin
+					$display("ex-mux: AUIPC type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: AUIPC type rs1_mux_output don't pass");
+				end
+			end
+
+			JAL: begin
+				JAL_rs1_mux_output: assert (rs1_mux_out == pc) begin
+					$display("ex-mux: JAL type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: JAL type rs1_mux_output don't pass");
+				end				
+			end
+
+			JALR: begin
+				JALR_rs1_mux_output: assert (rs1_mux_out == pc) begin
+					$display("ex-mux: JALR type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: JALR type rs1_mux_output don't pass");
+				end				
+			end
+
+			LOAD: begin
+				LOAD_rs1_mux_output: assert (rs1_mux_out == rs1) begin
+					$display("ex-mux: LOAD type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: LOAD type rs1_mux_output don't pass");
+				end					
+			end
+
+			STORE: begin
+				STORE_rs1_mux_output: assert (rs1_mux_out == rs1) begin
+					$display("ex-mux: STORE type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: STORE type rs1_mux_output don't pass");
+				end					
+			end
+
+			MEM: begin
+				MEM_rs1_mux_output: assert (rs1_mux_out == NULL) begin
+					$display("ex-mux: MEM type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: MEM type rs1_mux_output don't pass");
+				end					
+			end
+
+			SYS: begin
+				SYS_rs1_mux_output: assert (rs1_mux_out == NULL) begin
+					$display("ex-mux: SYS type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: SYS type rs1_mux_output don't pass");
+				end					
+			end
+
+		endcase
+	end
+
+
+
+
+	always_comb begin: opcode_rs2_mux_out_formal
+		case (opcode_formal)
+			R: begin
+				R_rs2_mux_output: assert (rs2_mux_out == rs2) begin
+					$display("ex-mux: R type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: R type rs2_mux_output don't pass");
+				end
+			end 
+
+			I: begin
+				I_rs2_mux_output: assert (rs2_mux_out == imm) begin
+					$display("ex-mux: I type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: I type rs2_mux_output don't pass");
+				end
+			end
+
+			B: begin
+				B_rs2_mux_output: assert (rs2_mux_out == rs2) begin
+					$display("ex-mux: B type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: B type rs2_mux_output don't pass, rs2_mux_out %d, rs2 %d", rs2_mux_out, rs2);
+				end
+			end
+
+			LUI: begin
+				LUI_rs2_mux_output: assert (rs2_mux_out == imm) begin
+					$display("ex-mux: LUI type rs1_mux_output pass");
+				end else begin
+					$display("ex-mux: LUI type rs1_mux_output don't pass");
+				end				
+			end
+
+			AUIPC: begin
+				AUIPC_rs2_mux_output: assert (rs2_mux_out == imm) begin
+					$display("ex-mux: AUIPC type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: AUIPC type rs2_mux_output don't pass");
+				end				
+			end
+
+			JAL: begin
+				JAL_rs2_mux_output: assert ((rs2_mux_out == imm) && (imm == 4)) begin
+					$display("ex-mux: JAL type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: JAL type rs2_mux_output don't pass");
+				end						
+			end
+
+			JALR: begin
+				JALR_rs2_mux_output: assert ((rs2_mux_out == imm) && (imm == 4)) begin
+					$display("ex-mux: JALR type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: JALR type rs2_mux_output don't pass");
+				end				
+			end
+
+			LOAD: begin
+				LOAD_rs2_mux_output: assert (rs2_mux_out == imm) begin
+					$display("ex-mux: LOAD type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: LOAD type rs2_mux_output don't pass");
+				end					
+			end
+
+			STORE: begin
+				STORE_rs2_mux_output: assert (rs2_mux_out == imm) begin
+					$display("ex-mux: STORE type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: STORE type rs2_mux_output don't pass");
+				end					
+			end
+
+			MEM: begin
+				MEM_rs2_mux_output: assert (rs2_mux_out == NULL) begin
+					$display("ex-mux: MEM type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: MEM type rs2_mux_output don't pass");
+				end					
+			end
+
+			SYS: begin
+				SYS_rs2_mux_output: assert (rs2_mux_out == NULL) begin
+					$display("ex-mux: SYS type rs2_mux_output pass");
+				end else begin
+					$display("ex-mux: SYS type rs2_mux_output don't pass");
+				end					
+			end
+
+		endcase
+	end
 
 ////////////////////////////////////////////////////////////////////////R type rs1_sel = 2'b10, rs2_sel = 2'b10
-	property R_B_a_rs1_mux;
-		(((opcode_formal == R) || (opcode_formal == B)) 
-						|-> ((rs1_mux_out == rs1)));
-	endproperty
+	/*
 
-	assert property(R_B_a_rs1_mux); //make sure R-type rs1_mux_out is rs1.....
-
-	property R_B_b_rs2_mux;
-		(((opcode_formal == R) || (opcode_formal == B)) 
-						|-> (rs2_mux_out == rs2));
-	endproperty
-
-	assert property(R_B_b_rs2_mux); // make sure R-type rs2_mux_out is rs2.....
 /////////////////////////////////////////////////////////////////////////I type rs1_sel=2'b10, imm_sel = 2'b11
 	property I_STORE_LOAD_a_rs1_mux;
 		(((opcode_formal == I) || (opcode_formal == STORE) || (opcode_formal == LOAD)) 
@@ -202,46 +400,8 @@ module ex_mux (
 
 	assert property(I_STORE_LOAD_b_rs2_mux); // make sure R-type rs2_mux_out is rs2.....
 /////////////////////////////////////////////////////////////////////// LUI: rs1_sel = null_sel,  rs2=imm_sel
-	property LUI_a_rs1_mux;
-		((opcode_formal == LUI) 
-						|-> (rs1_mux_out == NULL));
-	endproperty
-
-	assert property(LUI_a_rs1_mux);
-
-	property LUI_b_rs2_mux;
-		((opcode_formal == LUI) |-> (rs2_mux_out == imm));
-	endproperty
-
-	assert property(LUI_b_rs2_mux);
 /////////////////////////////////////////////////////////////////////////JAL, JALR rs1_sel = pc, rs2_sel = imm, imm =4
-	property JAL_JALR_a_rs1_mux;
-		(((opcode_formal == JAL) || (opcode_formal == JALR))
-						|-> (rs1_mux_out == pc));
-	endproperty
-
-	assert property(JAL_JALR_a_rs1_mux);
-
-	property JAL_JALR_b_rs2_mux;
-		(((opcode_formal == JAL) || (opcode_formal == JALR))
-						|-> ((rs2_mux_out == imm) && (imm == 4)));
-	endproperty
-
-	assert property(JAL_JALR_b_rs2_mux);
-///////////////////////////////////////////////////////////////////
-	property AUIPC_a_rs1_mux;
-		((opcode_formal == AUIPC)
-						|-> (rs1_mux_out == pc));
-	endproperty
-
-	assert property(AUIPC_a_rs1_mux);
-
-	property AUIPC_b_rs2_mux;
-		((opcode_formal == AUIPC)
-						|-> (rs2_mux_out == imm));
-	endproperty
-
-	assert property(AUIPC_b_rs2_mux);
+////
 ////////////////////////////////////////////////////////////////////
 	property MEM_SYS_output_mux;
 		((opcode_formal == MEM) || (opcode_formal == SYS)) 
