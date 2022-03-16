9713371b75d733a3df5b8e764580ba0b0b8a91dd
Luke <shuaige19981012@gmail.com>
2022-03-15 22:15:14 -0400

    recursion test

--- sim/test_gen/test.c
+++ sim/test_gen/test.c
@@ -1,22 +1,36 @@
 #define TB
 #include <stdint.h>
+#include <stdlib.h>
 
 #ifndef TB
 #include <stdio.h>
-#include <stdlib.h>
 #endif
 
 void swap(int*, int*);
 void bubbleSort(int*, int);
 void merge(int*, int, int, int);
 void mergeSort(int*, int, int);
+void swap(int*, int*);
+int partition (int*, int, int);
+void quickSort(int*, int, int);
+
 
 int main() {
-	int arr[] = {64, 34, 25, 12, 22, 11};
-	int sorted[] = {11, 12, 22, 25, 34, 64};
-	int n = sizeof(arr)/sizeof(arr[0]);
-    // bubbleSort(arr, n);
+	int n = 100;
+	int arr[n];
+	int sorted[n];
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand();
+	}
+
+	for (int i = 0; i < n; i++) {
+		sorted[i] = arr[i];
+	}
+
+	//bubbleSort(sorted, n);
 	mergeSort(arr, 0, n - 1);
+	quickSort(sorted, 0, n - 1);
 	
 	for (int i = 0; i < n; i++) {
 		if (arr[i] != sorted[i]) {
@@ -49,9 +63,9 @@ int main() {
 
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
  
 // A function to implement bubble sort
@@ -60,73 +74,106 @@ void bubbleSort(int arr[], int n)
    int i, j;
    for (i = 0; i < n-1; i++)     
  
-       // Last i elements are already in place  
-       for (j = 0; j < n-i-1; j++)
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
+	   // Last i elements are already in place  
+	   for (j = 0; j < n-i-1; j++)
+		   if (arr[j] > arr[j+1])
+			  swap(&arr[j], &arr[j+1]);
 }
 
 void merge(int arr[], int l, int m, int r)
 {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
   
-    /* create temp arrays */
-    int L[n1], R[n2];
+	/* create temp arrays */
+	int L[n1], R[n2];
   
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
   
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
   
-    /* Copy the remaining elements of L[], if there
-    are any */
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
   
-    /* Copy the remaining elements of R[], if there
-    are any */
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
 }
   
 /* l is for left index and r is right index of the
 sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r)
 {
-    if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
-        int m = l + (r - l) / 2;
+	if (l < r) {
+		// Same as (l+r)/2, but avoids overflow for
+		// large l and h
+		int m = l + (r - l) / 2;
   
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
+		// Sort first and second halves
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
   
-        merge(arr, l, m, r);
-    }
+		merge(arr, l, m, r);
+	}
+}
+
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot
+	int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
+ 
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than the pivot
+		if (arr[j] < pivot)
+		{
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		int pi = partition(arr, low, high);
+ 
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
 }
