111d851edb7b5f8a53c941ecea21858a9380532b
Bald Badger <32609230+Bald-Badger@users.noreply.github.com>
2022-04-06 21:50:00 -0400

    bruh

--- sw/main.c
+++ sw/main.c
@@ -67,8 +67,8 @@ void touch () {
 	printf("touching PA: %p\n", (void*)(mem_address + off));
 	printf("touching VA: %p\n", virtual_base + off);
 	usleep(100);
-	//write_sdram((virtual_base + off), data);
-	*((uint32_t *)virtual_base + off) = data;
+	write_sdram((virtual_base + off), data);
+	//*((uint32_t *)virtual_base + off) = data;
 	usleep(100);
 	//int32_t x = *((uint32_t *)virtual_base + off);
 	return;
