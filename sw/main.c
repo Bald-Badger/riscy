3ddf8f490a0ec818d553c10f3fa0e037f7262ceb
Luke <shuaige19981012@gmail.com>
2022-04-09 01:29:40 -0400

    sanity check

--- sw/main.c
+++ sw/main.c
@@ -11,17 +11,19 @@ const uint32_t h2f_lw_base = (unsigned int) ALT_LWFPGASLVS_OFST;
 const uint32_t h2f_base = (unsigned int) 0xC0000000;
 
 // sdram define
-const uint32_t sdram_range		= 0x03ffffff;	// 0x0 - 0x3ffffff
+const uint32_t sdram_range		= 0x03FFFFFF;	// 0x0 - 0x3ffffff
 const uint32_t offset_sdram		= 0x00000000;	// offset from bridge
 const uint32_t sdram_size_byte	= 0x04000000;	// 512Mb
 const uint32_t sdram_size_word	= 0x01000000;	// 64MB
 
 
 // 7-seg display define
-const uint32_t seg_range		= 0x0000001f;	// 0x0 - 0x1f
+const uint32_t seg_range		= 0x0000001F;	// 0x0 - 0x1f
 const uint32_t offset_seg		= 0x04000000;	// offset from bridge
 const uint32_t seg_size_byte	= 0x00000020;	// 32 bytes
 const uint32_t seg_size_word	= 0x00000008;	// 8 words (6 needed)
+const uint32_t seg_addr_mask	= 0xFFFFFFFC;	// word-align
+const uint32_t seg_data_mask	= 0x0000000F;	// only first byte valid
 
 
 void* map_addr (int pa_base, int size_byte) {
@@ -57,17 +59,20 @@ void* map_addr (int pa_base, int size_byte) {
 	return (return_va);
 }
 
-void* init_sdram() {
-	uint32_t sdram_pa_base	= h2f_base + offset_sdram;	// PA of sdram from HPS's perspective
-	return map_addr (sdram_pa_base, sdram_size_byte);
-}
-
 int unmap_addr (void* vp_base, u_int32_t unmap_size_byte) {
 	if( munmap( vp_base, unmap_size_byte ) != 0 ) {
 		printf( "ERROR: munmap() failed...\n" );
 		return( -1 );
 	}
 	return (0);
+}
+
+
+// SDRAM controlling functions
+
+void* init_sdram() {
+	uint32_t sdram_pa_base	= h2f_base + offset_sdram;	// PA of sdram from HPS's perspective
+	return map_addr (sdram_pa_base, sdram_size_byte);
 }
 
 int clean_sdram (void* vp_base) {
@@ -97,9 +102,34 @@ int touch_sdram (void* base, uint32_t off) {
 	}
 }
 
+
+// 7-Seg controlling functions
+
+void* init_seg() {
+	uint32_t seg_pa_base = h2f_base + offset_seg;	// PA of sdram from HPS's perspective
+	return map_addr (seg_pa_base, seg_size_byte);
+}
+
+int clean_seg (void* vp_base) {
+	return unmap_addr (vp_base, seg_size_byte);
+}
+
+void set_seg (uint32_t vp, int index, int number) {
+	vp = (uint32_t)vp & seg_addr_mask;
+	uint32_t hex_seg_digit = number & seg_data_mask;
+	vp = (uint32_t)vp + (index << 2);
+	*(uint32_t*)vp = hex_seg_digit;
+}
+
+
 int main () {
-	void* sdram_vp = init_sdram();
-	touch_sdram(sdram_vp, 0xffffff);
-	clean_sdram(sdram_vp);
+	//void* sdram_vp = init_sdram();
+	//touch_sdram(sdram_vp, 0xffffff);
+	//clean_sdram(sdram_vp);
+
+	void* seg_vp = init_seg();
+	set_seg (seg_vp, 1, 0xC);
+	clean_seg(seg_vp);
+
 	return( 0 );
 }
